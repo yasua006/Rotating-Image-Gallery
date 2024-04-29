0f066e291821402f7a57853029f3667866b99a11
Yasen <yasua006@osloskolen.no>
2024-04-29 10:51:58 +0200

    doesn't work
    add

---
+++ rotating image gallery/script.js
@@ -0,0 +1,32 @@
+const imgconElem = document.querySelector(".image-container");
+
+const prevEl = document.getElementById("prev");
+const nextEl = document.getElementById("next");
+
+let x = 0;
+let timer;
+
+function updateGallery()
+{
+    imgconElem.style.transform = 'perspective(1000px) rotateY(${x}deg)';
+    timer = setTimeout(()=> {
+        x = x - 45;
+        updateGallery();
+    }, 3000);
+}
+
+updateGallery()
+
+prevEl.addEventListener("click", ()=> {
+    x = x + 45;
+
+    clearTimeout(timer);
+    updateGallery();
+});
+
+nextEl.addEventListener("click", ()=> {
+    x = x - 45;
+
+    clearTimeout(timer);
+    updateGallery();
+});
